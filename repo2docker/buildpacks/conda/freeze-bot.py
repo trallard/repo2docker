a699ae9cf4b4b3216689589b548e093f7384da0d
Tania Allard <taniar.allard@gmail.com>
2020-06-09 15:49:33 +0100

    :sparkles: Initial PR comment check

--- repo2docker/buildpacks/conda/freeze-bot.py
+++ repo2docker/buildpacks/conda/freeze-bot.py
@@ -1,14 +1,28 @@
-from pathlib import Path
-import requests
 import json
-import time
-from github3 import GitHub
 
 if __name__ = "__main__":
 
     bot_phrase = os.getenv('BOT_PHRASE')
-
     assert bot_phrase = "Error: you have not provided a BOT_PHRASE"
 
     owner, repository = os.getenv('GITHUB_REPOSITORY').split("/")
+
+    # load the event payload to check details
+
+    event_payload = json.load("GITHUB_EVENT_PATH")
+
+    comment_text = event_payload['comment']
+    issue_data = event_payload['issue']
+
+    trigger_action = False
+
+    if bot_phrase in comment_text['body'] AND 'pull_request' in issue_data:
+        trigger_action = True 
+
+        print("Trigger phrase detected -- triggering refreeze")
+
+    
+    print(f"::set-output name=PULL_REQUEST_NUMBER::{issue_data['number']}")
+    print(f"::set-output name=TRIGGER_ACTION::{trigger_action}")
+
 
